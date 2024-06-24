97e886169962cae3b1fa24c87632792b7ff98d88
zillurahman10 <zillur.hero40@gmail.com>
2024-06-24 16:42:24 +0600

    designing done

--- app.js
+++ app.js
@@ -11,21 +11,22 @@ const loadServices = () => {
       const parent = document.getElementById("service-container");
       const li = document.createElement("li");
       li.innerHTML = `
-        <div class="card shadow h-100">
-                  <div class="ratio ratio-16x9">
+                <div style="width: 350px; height: 450px; border-radius: 20px" class="shadow">
+                  <div>
                     <img
                       src=${service.image}
-                      class="card-img-top"
+                      style="width: 300px; height: 200px; border-radius: 20px"
+                      class="mt-4"
                       loading="lazy"
                       alt="..."
                     />
                   </div>
-                  <div class="card-body p-3 p-xl-5">
-                    <h3 class="card-title h5">${service.name}</h3>
-                    <p class="card-text">
-                      ${service.description.slice(0, 140)}
+                  <div">
+                    <h3 class="mt-3">${service.name}</h3>
+                    <p class="w-75 mx-auto">
+                      ${service.description.slice(0, 120)}
                     </p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                    <a href="#" class="learn-more">Learn more</a>
                   </div>
                 </div>
         `;
@@ -67,24 +68,13 @@ const loadServices = () => {
       const div = document.createElement("div");
       div.classList.add("doc-card");
       div.innerHTML = `
-          <img class="doc-img" src=${doctor.image} alt="" />
-                <h4>${doctor?.full_name}</h4>
+          <img class="doc-img mt-3" src=${doctor.image} alt="" />
+                <h5 style="color: #007E85" class="mt-2">${doctor?.full_name}</h5>
                 <h6>${doctor?.designation[0]}</h6>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
                   numquam!
                 </p>
-               
-                <p>
-                
-                ${doctor?.specialization?.map((item) => {
-                  return `<button>${item}</button>`;
-                })}
-                </p>
-  
-                <button > <a target="_blank" href="docDetails.html?doctorId=${
-                  doctor.id
-                }">Details</a> </button>
           `;
   
       parent.appendChild(div);
